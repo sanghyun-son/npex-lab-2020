3253bd2dbf114ab32006f5ee9bd45543478ef4a6
Sanghyun Son <thstkdgus35@snu.ac.kr>
2020-09-23 10:55:50 +0900

    add resnet model

--- src/model/resnet.py
+++ src/model/resnet.py
@@ -1,5 +1,7 @@
 import torch
 from torch import nn
+from model import common
+
 
 class Net(nn.Module):
     '''
@@ -13,10 +15,13 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 64, 3, padding=1)
         self.relu1 = nn.ReLU(inplace=True)
 
+        '''
         self.conv2 = nn.Conv2d(64, 64, 3, padding=1)
         self.relu2 = nn.ReLU(inplace=True)
 
         self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
+        '''
+        self.res = common.ResBlock(64)
         self.relu3 = nn.ReLU(inplace=True)
 
         self.conv4 = nn.Conv2d(64, 3, 3, padding=1)
@@ -35,10 +40,13 @@ class Net(nn.Module):
         r = self.conv1(x)
         r = self.relu1(r)
 
+        '''
         r = self.conv2(r)
         r = self.relu2(r)
 
         r = self.conv3(r)
+        '''
+        r = self.res(r)
         r = self.relu3(r)
 
         r = self.conv4(r)
